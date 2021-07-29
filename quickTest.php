26535486d77b4a84671851d0f2edecb11c7c9e5e
Jerry Li <84467756+jerryo0@users.noreply.github.com>
2021-07-28 23:06:25 -0400

    Add files via upload

---
+++ quickTest.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'test1.php';
+//quick test.
+$data1 = 'jerry oqiwuehro weiur weriu eriuh weriouh ri<werw/ertwpoertw r>wertwerptower]t[weprt/w[ert/w[erpo/tw24524';
+$data2 = 'Li';
+$data3 = 'is';
+$data4 = 'my';
+$data5 = 'name';
+// echo 'Original hash:<br>' . ProofOfwork::gethash($data) . '<hr>';
+// $result = ProofOfWork::findNonce($data);
+// echo '<br>' . $result;
+
+// $block1 = new Block($data1, null);
+// $block1->setBlockNum(1);
+
+
+// $block2 = new Block($data2, $block1);
+// $block2->setBlockNum(2);
+
+
+// $block3 = new Block($data3, $block2);
+// $block3->setBlockNum(3);
+
+
+// $block4 = new Block($data4, $block3);
+// $block4->setBlockNum(4);
+
+
+
+// echo '<div id="blocks">' . $block1->__toString() . '<br>' . 
+//       $block2->__toString() . '<br>' . $block3->__toString() . 
+//      '<br>' . $block4->__toString() . '</div>';
+
+$block = new BlockChain($data1, null);
+$block->add($data2, 'jerry');
+$block->add($data3, 'jerry');
+$block->add($data4, 'jerry');
+// print $block . "\n";
+// [$block->blocks[1], $block->blocks[3]] = [$block->blocks[3], $block->blocks[1]];
+print $block->isValid();
+print $block . "\n" ;
+// var_dump($block);
